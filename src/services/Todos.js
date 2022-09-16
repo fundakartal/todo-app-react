77ab8182c07dfd0fda037080bb8cfe32439b20df
fundakartal <kartalfunda01@gmail.com>
2022-09-17 01:24:22 +0300

    Edit todo service

--- src/services/Todos.js
+++ src/services/Todos.js
@@ -3,24 +3,21 @@ import axios from 'axios'
 const baseUrl = process.env.REACT_APP_URL
 
 const getAll = async () => {
-  const request = axios.get(baseUrl)
-  const response = await request
-  return response.data
+  const res = await axios.get(baseUrl)
+  return res.data
 }
+
 const create = async (newObject) => {
-  const request = axios.post(baseUrl, newObject)
-  const response = await request
-  return response.data
+  const res = await axios.post(baseUrl, newObject)
+  return res.data
 }
 const update = async (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  const response = await request
-  return response.data
+  const res = await axios.put(`${baseUrl}/${id}`, newObject)
+  return res.data
 }
 const deleteItem = async (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`)
-  const response = await request
-  return response.data
+  const res = await axios.delete(`${baseUrl}/${id}`)
+  return res.data
 }
 
 const todoService = {
